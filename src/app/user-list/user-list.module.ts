7901cf7049efb8d29cf4296998bcfd27b7908574
mfurqaann <furqan1061@gmail.com>
2024-12-15 10:26:02 +0700

    refactor(userlist): refactor user list component & module

--- src/app/user-list/user-list.module.ts
+++ src/app/user-list/user-list.module.ts
@@ -4,10 +4,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { TableModule } from 'primeng/table';
 import { UserlistService } from './shared/userlist.service';
 import { UserListComponent } from './user-list.component';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [UserListComponent],
-  imports: [CommonModule, HttpClientModule, TableModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    TableModule,
+    RouterModule,
+    ButtonModule,
+  ],
   providers: [UserlistService],
 })
 export class UserListModule {}
